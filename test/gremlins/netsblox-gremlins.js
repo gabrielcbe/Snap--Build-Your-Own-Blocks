da46c8748601ae323b3b512ad10743b5daca2c61
Gordon Stein <gstein@ltu.edu>
2018-07-31 15:17:33 -0500

    Move gremlins to their own file

---
+++ test/gremlins/netsblox-gremlins.js
@@ -0,0 +1,23 @@
+/**
+ * Randomly changes the selected category
+ */
+const categoryChangeGremlin = function() {
+    let categoryNames = driver.getCategoryNames();
+    driver.selectCategory(categoryNames[Math.floor(Math.random() * categoryNames.length)]);
+};
+
+/**
+ * Randomly changes the name of the project to a Unicode string
+ */
+const projectNameChangeGremlin = function() {
+    let newName = "";
+    let newLength = Math.random() * 20;
+
+    for (let i = 0; i < newLength; i++) {
+        newName += String.fromCharCode("0x" + Math.floor(Math.random() * 65503 + 32).toString(16));
+    }
+
+    driver.setProjectName(newName);
+    driver.selectTab('scripts');
+};
+
